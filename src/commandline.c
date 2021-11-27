e196735cf389f1ae09efa97c10cfea9e975dabda
SOUMARE cheikou <cheikou.s99@gmail.com>
2021-11-27 23:20:40 +0100

    commandline.c

---
+++ src/commandline.c
@@ -0,0 +1,52 @@
+#include "../include/commandline.h"
+
+/* La fonction commandline_from_arguments récupére la commande à exécute dans argv et l'écrit dans
+ dest. Renvoie 0 en cas de succès sinon -1*/
+int commandline_from_arguments(struct commandline *dest, int argc, char **argv) {
+    int c = -1;
+    for(int i=0; i<argc; i++){
+        if(strcmp("-c", argv[i])==0){
+            c = i+1;
+            break;
+        }
+    }
+    if(c==-1){
+        return -1;
+    }
+    if(!(strcmp("-d", argv[c])==0 || strcmp("-H", argv[c])==0 || strcmp("-m", argv[c])==0)){
+        char *command[argc-c];
+        for (int i=0; i<argc-c; ++i) {
+            dest->ARGV[i] = malloc(sizeof(char)*strlen(argv[c+i]));
+            if(dest->ARGV[i]==NULL){
+                return -1;
+            }
+            strcpy(dest->ARGV[i],argv[c+i]);
+        }
+        dest->ARGC = argc - c;
+        return 0;
+    }
+    while(strcmp("-d", argv[c])==0 || strcmp("-H", argv[c])==0 || strcmp("-m", argv[c])==0){
+        c = c+2;
+    }
+    for (int i=0; i<argc-c; i++) {
+        dest->ARGV[i] = malloc(sizeof(char)*strlen(argv[c+i]));
+        if(dest->ARGV[i]==NULL){
+            return -1;
+        }
+        strcpy(dest->ARGV[i],argv[c+i]);
+    }
+    dest->ARGC = argc - c;
+    return 0;
+}
+void free_commandline(struct commandline *cl){
+    for(int i=0; i<cl->ARGC; i++){
+        free(cl->ARGV[i]);
+    }
+    free(cl->ARGV);
+}
+void print_commandline(struct commandline *cl){
+    for(int i=0; i<cl->ARGC; i++){
+        printf("%s ", cl->ARGV[i]);
+    }
+    printf("\n");
+}
